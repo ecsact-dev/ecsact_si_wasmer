12135c3240b646fbf602ad8af6d6a1c7aa84cd34
Ezekiel Warren <zekewarren@gmail.com>
2022-10-21 09:35:17 -0700

    Formatting

--- wasm_ecsact_system_execution.cc
+++ wasm_ecsact_system_execution.cc
@@ -8,59 +8,50 @@
 #include "wasm_ecsact_pointer_map.hh"
 
 namespace {
-	std::unordered_map<ecsact_system_execution_context*, wasm_memory_t*> mem_map;
 
-	ecsact_system_execution_context* get_execution_context
-		( const wasm_val_t& val
-		)
-	{
-		assert(val.kind == WASM_I32);
-		return ecsactsi_wasm::as_host_pointer<ecsact_system_execution_context>(
-			val.of.i32
-		);
-	}
+std::unordered_map<ecsact_system_execution_context*, wasm_memory_t*> mem_map;
 
-	template<typename EcsactID>
-	EcsactID ecsact_id_from_wasm_i32
-		( const wasm_val_t&  val
-		)
-	{
-		assert(val.kind == WASM_I32);
-		return static_cast<EcsactID>(val.of.i32);
-	}
-
-	void* get_void_ptr
-		( const wasm_val_t&  val
-		, wasm_memory_t*     memory
-		)
-	{
-		assert(val.kind == WASM_I32);
-		if(val.of.i32 == 0) return nullptr;
-		
-		auto mem_bytes = wasm_memory_data(memory);
-		// the i32 val is an index of the wasm memory data
-		return mem_bytes + val.of.i32;
-	}
-
-	const void* get_const_void_ptr
-		( const wasm_val_t&  val
-		, wasm_memory_t*     memory
-		)
-	{
-		assert(val.kind == WASM_I32);
-		if(val.of.i32 == 0) return nullptr;
-
-		auto mem_bytes = wasm_memory_data(memory);
-		// the i32 val is an index of the wasm memory data
-		return mem_bytes + val.of.i32;
-	}
+ecsact_system_execution_context* get_execution_context(const wasm_val_t& val) {
+	assert(val.kind == WASM_I32);
+	return ecsactsi_wasm::as_host_pointer<ecsact_system_execution_context>(
+		val.of.i32
+	);
 }
 
-void set_wasm_ecsact_system_execution_context_memory
-	( ecsact_system_execution_context*  ctx
-	, wasm_memory_t*                    memory
-	)
-{
+template<typename EcsactID>
+EcsactID ecsact_id_from_wasm_i32(const wasm_val_t& val) {
+	assert(val.kind == WASM_I32);
+	return static_cast<EcsactID>(val.of.i32);
+}
+
+void* get_void_ptr(const wasm_val_t& val, wasm_memory_t* memory) {
+	assert(val.kind == WASM_I32);
+	if(val.of.i32 == 0) {
+		return nullptr;
+	}
+
+	auto mem_bytes = wasm_memory_data(memory);
+	// the i32 val is an index of the wasm memory data
+	return mem_bytes + val.of.i32;
+}
+
+const void* get_const_void_ptr(const wasm_val_t& val, wasm_memory_t* memory) {
+	assert(val.kind == WASM_I32);
+	if(val.of.i32 == 0) {
+		return nullptr;
+	}
+
+	auto mem_bytes = wasm_memory_data(memory);
+	// the i32 val is an index of the wasm memory data
+	return mem_bytes + val.of.i32;
+}
+
+} // namespace
+
+void set_wasm_ecsact_system_execution_context_memory(
+	ecsact_system_execution_context* ctx,
+	wasm_memory_t*                   memory
+) {
 	if(memory == nullptr) {
 		mem_map.erase(ctx);
 	} else {
@@ -68,11 +59,10 @@ void set_wasm_ecsact_system_execution_context_memory
 	}
 }
 
-wasm_trap_t* wasm_ecsact_system_execution_context_action
-	( const wasm_val_vec_t*  args
-	, wasm_val_vec_t*        results
-	)
-{
+wasm_trap_t* wasm_ecsact_system_execution_context_action(
+	const wasm_val_vec_t* args,
+	wasm_val_vec_t*       results
+) {
 	auto ctx = get_execution_context(args->data[0]);
 	auto memory = mem_map.at(ctx);
 
@@ -84,11 +74,10 @@ wasm_trap_t* wasm_ecsact_system_execution_context_action
 	return nullptr;
 }
 
-wasm_trap_t* wasm_ecsact_system_execution_context_add
-	( const wasm_val_vec_t*  args
-	, wasm_val_vec_t*        results
-	)
-{
+wasm_trap_t* wasm_ecsact_system_execution_context_add(
+	const wasm_val_vec_t* args,
+	wasm_val_vec_t*       results
+) {
 	auto ctx = get_execution_context(args->data[0]);
 	auto memory = mem_map.at(ctx);
 
@@ -101,11 +90,10 @@ wasm_trap_t* wasm_ecsact_system_execution_context_add
 	return nullptr;
 }
 
-wasm_trap_t* wasm_ecsact_system_execution_context_remove
-	( const wasm_val_vec_t*  args
-	, wasm_val_vec_t*        results
-	)
-{
+wasm_trap_t* wasm_ecsact_system_execution_context_remove(
+	const wasm_val_vec_t* args,
+	wasm_val_vec_t*       results
+) {
 	ecsact_system_execution_context_remove(
 		get_execution_context(args->data[0]),
 		ecsact_id_from_wasm_i32<ecsact_component_like_id>(args->data[1])
@@ -113,11 +101,10 @@ wasm_trap_t* wasm_ecsact_system_execution_context_remove
 	return nullptr;
 }
 
-wasm_trap_t* wasm_ecsact_system_execution_context_get
-	( const wasm_val_vec_t*  args
-	, wasm_val_vec_t*        results
-	)
-{
+wasm_trap_t* wasm_ecsact_system_execution_context_get(
+	const wasm_val_vec_t* args,
+	wasm_val_vec_t*       results
+) {
 	auto ctx = get_execution_context(args->data[0]);
 	auto memory = mem_map.at(ctx);
 
@@ -130,11 +117,10 @@ wasm_trap_t* wasm_ecsact_system_execution_context_get
 	return nullptr;
 }
 
-wasm_trap_t* wasm_ecsact_system_execution_context_update
-	( const wasm_val_vec_t*  args
-	, wasm_val_vec_t*        results
-	)
-{
+wasm_trap_t* wasm_ecsact_system_execution_context_update(
+	const wasm_val_vec_t* args,
+	wasm_val_vec_t*       results
+) {
 	auto ctx = get_execution_context(args->data[0]);
 	auto memory = mem_map.at(ctx);
 
@@ -147,11 +133,10 @@ wasm_trap_t* wasm_ecsact_system_execution_context_update
 	return nullptr;
 }
 
-wasm_trap_t* wasm_ecsact_system_execution_context_has
-	( const wasm_val_vec_t*  args
-	, wasm_val_vec_t*        results
-	)
-{
+wasm_trap_t* wasm_ecsact_system_execution_context_has(
+	const wasm_val_vec_t* args,
+	wasm_val_vec_t*       results
+) {
 	bool has_component = ecsact_system_execution_context_has(
 		get_execution_context(args->data[0]),
 		ecsact_id_from_wasm_i32<ecsact_component_like_id>(args->data[1])
@@ -163,11 +148,10 @@ wasm_trap_t* wasm_ecsact_system_execution_context_has
 	return nullptr;
 }
 
-wasm_trap_t* wasm_ecsact_system_execution_context_generate
-	( const wasm_val_vec_t*  args
-	, wasm_val_vec_t*        results
-	)
-{
+wasm_trap_t* wasm_ecsact_system_execution_context_generate(
+	const wasm_val_vec_t* args,
+	wasm_val_vec_t*       results
+) {
 	auto ctx = get_execution_context(args->data[0]);
 	auto memory = mem_map.at(ctx);
 
@@ -180,14 +164,13 @@ wasm_trap_t* wasm_ecsact_system_execution_context_generate
 	return nullptr;
 }
 
-wasm_trap_t* wasm_ecsact_system_execution_context_parent
-	( const wasm_val_vec_t*  args
-	, wasm_val_vec_t*        results
-	)
-{
-	auto parent = ecsact_system_execution_context_parent(
-		get_execution_context(args->data[0])
-	);
+wasm_trap_t* wasm_ecsact_system_execution_context_parent(
+	const wasm_val_vec_t* args,
+	wasm_val_vec_t*       results
+) {
+	auto parent =
+		ecsact_system_execution_context_parent(get_execution_context(args->data[0])
+		);
 
 	results->data[0].kind = WASM_I32;
 	results->data[0].of.i32 = ecsactsi_wasm::as_guest_pointer(
@@ -197,46 +180,43 @@ wasm_trap_t* wasm_ecsact_system_execution_context_parent
 	return nullptr;
 }
 
-wasm_trap_t* wasm_ecsact_system_execution_context_same
-	( const wasm_val_vec_t*  args
-	, wasm_val_vec_t*        results
-	)
-{
+wasm_trap_t* wasm_ecsact_system_execution_context_same(
+	const wasm_val_vec_t* args,
+	wasm_val_vec_t*       results
+) {
 	bool same = ecsact_system_execution_context_same(
 		get_execution_context(args->data[0]),
 		get_execution_context(args->data[1])
 	);
-	
+
 	results->data[0].kind = WASM_I32;
 	results->data[0].of.i32 = same ? 1 : 0;
 
 	return nullptr;
 }
 
-wasm_trap_t* wasm_ecsact_system_execution_context_other
-	( const wasm_val_vec_t*  args
-	, wasm_val_vec_t*        results
-	)
-{
+wasm_trap_t* wasm_ecsact_system_execution_context_other(
+	const wasm_val_vec_t* args,
+	wasm_val_vec_t*       results
+) {
 	auto other = ecsact_system_execution_context_other(
 		get_execution_context(args->data[0]),
 		ecsact_id_from_wasm_i32<ecsact_entity_id>(args->data[1])
 	);
-	
+
 	results->data[0].kind = WASM_I32;
 	results->data[0].of.i32 = ecsactsi_wasm::as_guest_pointer(other);
 
 	return nullptr;
 }
 
-wasm_trap_t* wasm_ecsact_system_execution_context_entity
-	( const wasm_val_vec_t*  args
-	, wasm_val_vec_t*        results
-	)
-{
-	auto entity = ecsact_system_execution_context_entity(
-		get_execution_context(args->data[0])
-	);
+wasm_trap_t* wasm_ecsact_system_execution_context_entity(
+	const wasm_val_vec_t* args,
+	wasm_val_vec_t*       results
+) {
+	auto entity =
+		ecsact_system_execution_context_entity(get_execution_context(args->data[0])
+		);
 	results->data[0].kind = WASM_I32;
 	results->data[0].of.i32 = static_cast<int32_t>(entity);
 
--- wasm_ecsact_system_execution.h
+++ wasm_ecsact_system_execution.h
@@ -9,64 +9,64 @@
 #include <wasm.h>
 #include "ecsact/runtime/common.h"
 
-void set_wasm_ecsact_system_execution_context_memory
-	( ecsact_system_execution_context*  ctx
-	, wasm_memory_t*                    memory
-	);
+void set_wasm_ecsact_system_execution_context_memory(
+	ecsact_system_execution_context* ctx,
+	wasm_memory_t*                   memory
+);
 
-wasm_trap_t* wasm_ecsact_system_execution_context_action
-	( const wasm_val_vec_t*  args
-	, wasm_val_vec_t*        results
-	);
+wasm_trap_t* wasm_ecsact_system_execution_context_action(
+	const wasm_val_vec_t* args,
+	wasm_val_vec_t*       results
+);
 
-wasm_trap_t* wasm_ecsact_system_execution_context_add
-	( const wasm_val_vec_t*  args
-	, wasm_val_vec_t*        results
-	);
+wasm_trap_t* wasm_ecsact_system_execution_context_add(
+	const wasm_val_vec_t* args,
+	wasm_val_vec_t*       results
+);
 
-wasm_trap_t* wasm_ecsact_system_execution_context_remove
-	( const wasm_val_vec_t*  args
-	, wasm_val_vec_t*        results
-	);
+wasm_trap_t* wasm_ecsact_system_execution_context_remove(
+	const wasm_val_vec_t* args,
+	wasm_val_vec_t*       results
+);
 
-wasm_trap_t* wasm_ecsact_system_execution_context_get
-	( const wasm_val_vec_t*  args
-	, wasm_val_vec_t*        results
-	);
+wasm_trap_t* wasm_ecsact_system_execution_context_get(
+	const wasm_val_vec_t* args,
+	wasm_val_vec_t*       results
+);
 
-wasm_trap_t* wasm_ecsact_system_execution_context_update
-	( const wasm_val_vec_t*  args
-	, wasm_val_vec_t*        results
-	);
+wasm_trap_t* wasm_ecsact_system_execution_context_update(
+	const wasm_val_vec_t* args,
+	wasm_val_vec_t*       results
+);
 
-wasm_trap_t* wasm_ecsact_system_execution_context_has
-	( const wasm_val_vec_t*  args
-	, wasm_val_vec_t*        results
-	);
+wasm_trap_t* wasm_ecsact_system_execution_context_has(
+	const wasm_val_vec_t* args,
+	wasm_val_vec_t*       results
+);
 
-wasm_trap_t* wasm_ecsact_system_execution_context_generate
-	( const wasm_val_vec_t*  args
-	, wasm_val_vec_t*        results
-	);
+wasm_trap_t* wasm_ecsact_system_execution_context_generate(
+	const wasm_val_vec_t* args,
+	wasm_val_vec_t*       results
+);
 
-wasm_trap_t* wasm_ecsact_system_execution_context_parent
-	( const wasm_val_vec_t*  args
-	, wasm_val_vec_t*        results
-	);
+wasm_trap_t* wasm_ecsact_system_execution_context_parent(
+	const wasm_val_vec_t* args,
+	wasm_val_vec_t*       results
+);
 
-wasm_trap_t* wasm_ecsact_system_execution_context_same
-	( const wasm_val_vec_t*  args
-	, wasm_val_vec_t*        results
-	);
+wasm_trap_t* wasm_ecsact_system_execution_context_same(
+	const wasm_val_vec_t* args,
+	wasm_val_vec_t*       results
+);
 
-wasm_trap_t* wasm_ecsact_system_execution_context_other
-	( const wasm_val_vec_t*  args
-	, wasm_val_vec_t*        results
-	);
+wasm_trap_t* wasm_ecsact_system_execution_context_other(
+	const wasm_val_vec_t* args,
+	wasm_val_vec_t*       results
+);
 
-wasm_trap_t* wasm_ecsact_system_execution_context_entity
-	( const wasm_val_vec_t*  args
-	, wasm_val_vec_t*        results
-	);
+wasm_trap_t* wasm_ecsact_system_execution_context_entity(
+	const wasm_val_vec_t* args,
+	wasm_val_vec_t*       results
+);
 
-#endif//WASM_ECSACT_SYSTEM_EXECUTION__H
+#endif // WASM_ECSACT_SYSTEM_EXECUTION__H
