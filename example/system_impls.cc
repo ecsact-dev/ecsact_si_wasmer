c199b6e2386adce5484453b5998d86c6d26387a1
Ezekiel Warren <zekewarren@gmail.com>
2022-11-20 15:31:01 -0800

    using latest ecsact sdk in example

--- example/system_impls.cc
+++ example/system_impls.cc
@@ -1,6 +1,11 @@
 #include "example.ecsact.hh"
 #include "example.ecsact.systems.hh"
 
+void example__ExampleSystem(ecsact_system_execution_context* c_ctx) {
+	example::ExampleSystem::context ctx{ecsact::execution_context{c_ctx}};
+	example::ExampleSystem::impl(ctx);
+}
+
 void example::ExampleSystem::impl(context& ctx) {
 	auto comp = ctx.get<example::ExampleComponent>();
 	comp.num += 1;
