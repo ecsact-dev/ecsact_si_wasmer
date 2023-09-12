6078199a654bfe897bb8b149c5e5121e760d86f1
Ezekiel Warren <zekewarren@gmail.com>
2023-09-11 17:32:40 -0700

    chore: move tests to different folder

--- tests/minst_test_wasm.cc
+++
@@ -1,9 +0,0 @@
-
-#include "ecsact/runtime/common.h"
-
-ECSACT_IMPORT("mod", "minst_test_import_fn")
-auto minst_test_import_fn() -> void;
-
-ECSACT_EXPORT("minst_test_export_fn") auto minst_test_export_fn() -> void {
-	minst_test_import_fn();
-}
