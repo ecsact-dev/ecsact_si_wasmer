e206c0929d718bc188c68e4223f597a86365264b
Ezekiel Warren <ezekiel@seaube.com>
2024-04-22 09:07:45 -0700

    chore: formatting

--- test/wasi_test_iostream.cc
+++ test/wasi_test_iostream.cc
@@ -4,8 +4,6 @@
 #include <iostream>
 
 void wasi_test__WasiTestSystem(ecsact_system_execution_context*) {
-	std::cout //
-		<< "Hello from std::cout (1) "
-		<< " other thing\n";
+	std::cout << "Hello from std::cout (1) " << " other thing\n";
 	std::cout << "Hello from std::cout (2)\n";
 }
