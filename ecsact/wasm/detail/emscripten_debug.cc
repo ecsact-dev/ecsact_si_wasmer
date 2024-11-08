9bf7bf1b472f4e6da5145f4168b9ff85867903c7
Ezekiel Warren <ezekiel@seaube.com>
2024-11-07 17:30:04 -0800

    chore: add exceptiont to segfault handler

--- ecsact/wasm/detail/emscripten_debug.cc
+++ ecsact/wasm/detail/emscripten_debug.cc
@@ -1,15 +1,16 @@
 #include "ecsact/wasm/detail/emscripten_debug.hh"
+#include <stdexcept>
 
 auto ecsact::wasm::detail::emscripten_debug_segfault(
 	const wasm_val_vec_t* args,
 	wasm_val_vec_t*       results
 ) -> wasm_trap_t* {
-	return nullptr;
+	throw std::logic_error{"segfault"};
 }
 
 auto ecsact::wasm::detail::emscripten_debug_alignfault(
 	const wasm_val_vec_t* args,
 	wasm_val_vec_t*       results
 ) -> wasm_trap_t* {
-	return nullptr;
+	throw std::logic_error{"alignfault"};
 }
